173bc37e276d6dc7f60151acf39d40980bd2c7b6
zeckoH <37878668+zeckoh@users.noreply.github.com>
2023-05-27 23:40:23 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Lunar'
+description 'Atm robbery script by Lunar changed and translated by zeckoH'
+version '1.0.0'
+
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+shared_script '@es_extended/imports.lua'
